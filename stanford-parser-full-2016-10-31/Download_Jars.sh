4596af3b34f0333f85d030f15737711819facf5e
Lisa Barcelo <3ll3@users.noreply.github.com>
2017-06-14 00:17:46 -0700

    Create Download_Jars.sh
    The last few jars are really big and only need to be downloaded once to your github repo. So as not to take up space, I have included a link to the zip file in my public dropbox, it should be live in about an hour from the time of this upload.

---
+++ stanford-parser-full-2016-10-31/Download_Jars.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# This is the final download... it is all the .jar files from Dropbox that you will need to download just once into your local folder
+
+!curl -0 https://www.dropbox.com/s/t9uk4z1xznpo0jz/jars.zip
