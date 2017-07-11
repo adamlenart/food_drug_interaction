b0834d1f11c36a2872918fea4d39e046a1828a79
Lisa Barcelo <lisabarcelo@Lisas-MBP.attlocal.net>
2017-07-10 22:41:32 -0700

    cleaning up stanford parsing files

--- stanford-parser-full-2016-10-31/Download_Jars.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# This is the final download... it is all the .jar files from Dropbox that you will need to download just once into your local folder
-
-!curl -0 https://www.dropbox.com/s/t9uk4z1xznpo0jz/jars.zip
